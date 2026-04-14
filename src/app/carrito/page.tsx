2bae56c286732f2a7dcfe02ae3d570a610510f44
miguel <miguel@192.168.1.5>
2026-04-14 12:50:55 -0600

    prueba final

--- src/app/carrito/page.tsx
+++ src/app/carrito/page.tsx
@@ -13,10 +13,10 @@ export default function CarritoPage() {
 
   return (
     <main className="min-h-screen px-6 py-10">
-      <h1 className="text-3xl font-bold mb-6">Alex Miguel Carrito pipeline funcionando14426</h1>
+      <h1 className="text-3xl font-bold mb-6">Alex Miguel Prueba final Carrito pipeline funcionando14426</h1>
 
       {items.length === 0 && (
-        <p className="text-lg text-slate-500">Alex Miguel Tu carrito está vacío pipeline funcionando14426.</p>
+        <p className="text-lg text-slate-500">Alex Miguel prueba final Tu carrito está vacío pipeline funcionando14426.</p>
       )}
 
       {items.length > 0 && (

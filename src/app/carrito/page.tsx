e738fbcd71511229a7386b03c00d060d013a59ce
miguel <miguel@192.168.1.5>
2026-04-15 02:46:30 -0600

    test 154265

--- src/app/carrito/page.tsx
+++ src/app/carrito/page.tsx
@@ -13,10 +13,10 @@ export default function CarritoPage() {
 
   return (
     <main className="min-h-screen px-6 py-10">
-      <h1 className="text-3xl font-bold mb-6">Carrito 154264</h1>
+      <h1 className="text-3xl font-bold mb-6">Carrito 154265</h1>
 
       {items.length === 0 && (
-        <p className="text-lg text-slate-500">Tu carrito está vacío 154264.</p>
+        <p className="text-lg text-slate-500">Tu carrito está vacío 154265.</p>
       )}
 
       {items.length > 0 && (
